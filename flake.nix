dc41ae1662c4c760f3deba9f826ba605e99971cc
Jappie3 <jappie3+git@jappie.dev>
2025-07-08 20:19:29 +0200

    flake: format

--- flake.nix
+++ flake.nix
@@ -5,28 +5,36 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {nixpkgs, ...}: let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-      ] (system: function nixpkgs.legacyPackages.${system});
-  in {
-    devShells = forAllSystems (pkgs: {
-      default = pkgs.mkShell {
-        buildInputs = with pkgs; [rustfmt cargo libxkbcommon];
-      };
-    });
-    packages = forAllSystems (pkgs: rec {
-      default = wayfreeze;
-      wayfreeze = pkgs.rustPlatform.buildRustPackage {
-        name = "wayfreeze";
-        src = ./.;
-        cargoHash = "sha256-uzTT4WyR7kCL/HPu7JHGQqG9tbO1JGIW1Jtlza5lhPk=";
-        doCheck = true;
-        nativeBuildInputs = [];
-        buildInputs = with pkgs; [libxkbcommon];
-      };
-    });
-  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustfmt
+            cargo
+            libxkbcommon
+          ];
+        };
+      });
+      packages = forAllSystems (pkgs: rec {
+        default = wayfreeze;
+        wayfreeze = pkgs.rustPlatform.buildRustPackage {
+          name = "wayfreeze";
+          src = ./.;
+          cargoHash = "sha256-uzTT4WyR7kCL/HPu7JHGQqG9tbO1JGIW1Jtlza5lhPk=";
+          doCheck = true;
+          nativeBuildInputs = [ ];
+          buildInputs = with pkgs; [ libxkbcommon ];
+        };
+      });
+    };
 }
